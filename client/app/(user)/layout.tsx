656956f2a3d258a096b17b178f48d3b955f74575
Shahid Gillani <shahid5ssg@gmail.com>
2023-11-12 23:25:40 +0500

    Added poppins font as a local font using nextjs/local

--- client/app/(user)/layout.tsx
+++ client/app/(user)/layout.tsx
@@ -1,7 +1,48 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
+import '../globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/Poppins-Thin.ttf',
+      weight: '100'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ExtraLight.ttf',
+      weight: '200'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Light.ttf',
+      weight: '300'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Regular.ttf',
+      weight: '400'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Medium.ttf',
+      weight: '500'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-SemiBold.ttf',
+      weight: '600'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Bold.ttf',
+      weight: '700'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ExtraBold.ttf',
+      weight: '800'
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Black.ttf',
+      weight: '900'
+    },
+  ],
+  variable: '--font-poppins'
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,8 +55,8 @@ export default function UserRootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en"  className={`${poppins.variable} font-sans`}>
+      <body  className={`${poppins.className} font-sans`}>
         <h1>User layout</h1>
         {children}
       </body>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
