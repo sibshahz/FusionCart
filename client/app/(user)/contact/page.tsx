7fc206cd4adb5dd825ad401c49dd2497901e688e
Shahid Gillani <shahid5ssg@gmail.com>
2023-11-17 23:04:44 +0500

    Adding pages

---
+++ client/app/(user)/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div>Contact Page</div>
+  )
+}
+
+export default ContactPage
--- client/src/components/user/header/header.component.tsx
+++ client/src/components/user/header/header.component.tsx
@@ -1,15 +1,26 @@
-import React,{FC} from 'react'
+"use client"
+import React,{FC, useEffect} from 'react'
 import { MainNav } from '@/src/utils/user/navigations'
-import Link from 'next/link'
+import Link from 'next/link' 
+import { usePathname } from 'next/navigation'
+
 
 
 const Header:FC = () => {
+  const pathname = usePathname()
   return (
     <>
       {
         MainNav.map((item) => {
           return(
-            <Link href={item.url}>{item.title}</Link>
+            <Link 
+              href={item.url} 
+              className={
+                pathname === item.url ?
+                'mr-2 bg-slate-500' :
+                'mr-2'}>
+              {item.title}
+            </Link>
           )
         })
       }
