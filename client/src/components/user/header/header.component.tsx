eb7c0a7aa418704507f4a564f0b2e82b6fd9cb43
Shahid Gillani <shahid5ssg@gmail.com>
2023-11-20 22:47:54 +0500

    Added navigation settings and styles

--- client/src/components/user/header/header.component.tsx
+++ client/src/components/user/header/header.component.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation'
 import LayoutContainer from '../layout-container/laytout-container.component'
 import Image from 'next/image'
 import Logo from '../../../../public/imgs/logo.png'
+import { Account,Search,Heart,Cart } from '@/src/utils/user/icons/icons'
 
 
 
@@ -13,7 +14,7 @@ const Header:FC = () => {
   const pathname = usePathname()
   return (
     <LayoutContainer>
-    <div className='flex justify-between flex-wrap items-center h-24'>
+    <div className='flex justify-between flex-wrap items-center min-h-[100px] pt-2 pb-2'>
       <div className='header--logo-container flex items-center'>
         <div className="inline">
           <Image src={Logo.src} width={50} height={32} alt='Furniro Furnitures' />
@@ -21,17 +22,18 @@ const Header:FC = () => {
         <h1 className='font-bold text-3xl inline text-black font-heading'>Furniro</h1>
       </div>
       
-      <div className='header--nav-container'>
+      <div className='header--nav-container flex flex-wrap gap-16'>
         {
 
-          MainNav.map((item) => {
+          MainNav.map((item,index) => {
             return(
-              <Link 
+              <Link
+                key={`nav-item-${index}`} 
                 href={item.url} 
                 className={
                   pathname === item.url ?
-                  'mr-16 bg-slate-500 font-medium' :
-                  'mr-16 font-medium'}>
+                  'bg-gray-2 text-primary font-medium' :
+                  'font-medium'}>
                 {item.title}
               </Link>
             )
@@ -40,8 +42,11 @@ const Header:FC = () => {
         }
       </div>
       
-      <div className='header--icons-container'>
-        Icons
+      <div className='header--icons-container flex flex-wrap items-center gap-11'>
+        <img  className="inline hover:cursor-pointer" src={Account.src} alt="Account" />
+        <img  className="inline hover:cursor-pointer" src={Search.src} alt="Search" />
+        <img  className="inline hover:cursor-pointer" src={Heart.src} alt="Heart" />
+        <img  className="inline hover:cursor-pointer" src={Cart.src} alt="Cart" />        
       </div>
     </div>
     </LayoutContainer>
---
+++ client/src/utils/user/icons/icons.ts
@@ -0,0 +1,11 @@
+import Account from '../../../../public/icons-user/account.svg'
+import Cart from '../../../../public/icons-user/cart.svg'
+import Heart from '../../../../public/icons-user/heart.svg'
+import Search from '../../../../public/icons-user/search.svg'
+
+export {
+  Account,
+  Cart,
+  Heart,
+  Search
+}
