69f5d1bcf654cf681d2c765a1ee611712e55f6ea
Shahid Gillani <shahid5ssg@gmail.com>
2024-01-08 07:13:07 +0500

    Basic single product component page

---
+++ client/src/components/user/product/single-product.component.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Product } from '@/product/product.types';
+import LayoutContainer from '../layout-container/layout-container.component';
+
+type Props = {
+  product: Product;
+};
+
+const SingleProduct: React.FC<Props> = ({ product }) => {
+  const [selectedImg,setSelectedImg]=React.useState(0)
+  
+  const setIndex = React.useCallback((index) => {
+    setSelectedImg(index);
+  }, []);
+
+  return (
+    <>
+    <LayoutContainer bg='inherit'>
+    <div className='grid grid-cols-12 gap-20'>
+      <div className="col-span-5">
+        <div className='product--image-gallery flex flex-row gap-7'>
+          <div className='product--image-roller'>
+          <div className='rounded-lg flex flex-col gap-8 overflow-hidden'>
+            {product?.images?.map((img, index) => (
+              index !== selectedImg ? (
+                <div 
+                  onClick={() => setIndex(index)}
+                  key={index} 
+                  className='rounded-lg overflow-hidden flex-shrink-0 w-20 h-20 hover:cursor-pointer'>
+                  <img 
+                    src={`http://localhost:8080/${img?.imagePath}` || ''}
+                    alt={img?.imageAlt}
+                    className='w-full h-full object-cover'
+                  />
+                </div>
+              ) : (
+                null
+              )
+            ))}
+          </div>
+
+          </div>
+          <div className='product--image-selected overflow-hidden'>
+            <img 
+            className='w-full h-full object-cover'
+            src={`http://localhost:8080/${product?.images[selectedImg]?.imagePath}` || ''} alt={product?.images?.[selectedImg]?.imageAlt} />
+          </div>
+        </div>
+      </div>
+
+      <div className="col-span-7">
+        <div className='flex flex-col gap-4'>
+
+          <div className='product--title text-[42px] font-normal leading-normal'>{product?.name}</div>
+          <div className='product--price text-2xl font-medium text-gray-5'>Rs. {product?.salePrice} .00</div>
+          <div className='product--actions flex flex-row gap-5'>
+
+          <button className="bg-white text-xl font-normal py-4 px-12 rounded-2xl border border-black">
+            + Add to cart
+          </button>
+
+          <button className="bg-white text-xl font-normal py-4 px-12 rounded-2xl border border-black">
+            Buy now
+          </button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    </LayoutContainer>
+
+    <div>
+      <LayoutContainer bg='inherit'>
+        <div className='text-justify text-base font-normal text-gray-5 leading-normal'>
+          {product?.description}
+        </div>
+      </LayoutContainer>
+    </div>
+    </>
+  );
+};
+
+export default SingleProduct;
